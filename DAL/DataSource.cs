efdec6076ca12a50649d8c2f843596d9621c0243
Lucas Morales <moraleslucasjulian@gmail.com>
2019-01-08 02:01:39 -0300

    Listar Servicios
    listar servicios

---
+++ DAL/DataSource.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class DataSource
+    {
+            public SqlCommand getCommand()
+            {
+                SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["DigitalSupportDB"].ToString());
+                SqlCommand cmm = conexion.CreateCommand();
+                return cmm;
+            }
+
+            public SqlConnection getConexion()
+            {
+                SqlConnection conexion = new SqlConnection(ConfigurationManager.ConnectionStrings["DigitalSupportDB"].ToString());
+                return conexion;
+            }
+        }
+    }
+}
--- DAL/ServicioOT_DAL.cs
+++ DAL/ServicioOT_DAL.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,18 +10,48 @@ namespace DAL
 {
     static class ServicioOT_DAL
     {
-        DigitalSupportDBDataContext DataContextDB = new DigitalSupportDBDataContext();
-        private object otTable = new ORDENTRABAJO();
+        private static DataSource Dsource;
 
         public static List<ServicioOT> SelectAllServices()
         {
-            List<ServicioOT> serviosOT = new List<ServicioOT>();
-            
+            SqlConnection conexion;
+
+            conexion = Dsource.getConexion();
+            conexion.Open();
+
+            List<Libreta> libretas = new List<Libreta>();
+
+            SqlConnection conexion = new SqlConnection(ConfigurationManager.AppSettings["ConexionBaseDeDatos"]);
+
+            conexion.Open();
+
+            SqlCommand query = conexion.CreateCommand();
+
+            query.CommandText = "SELECT Libreta.ID_libreta, Libreta.NameLibreta, Libreta.ID_color FROM Usuario_Libreta INNER JOIN Libreta ON Usuario_Libreta.ID_libreta = Libreta.ID_libreta INNER JOIN Usuario ON Usuario_Libreta.ID_usuario = Usuario.ID_usuario WHERE Usuario.ID_usuario = @ID_usuario";
+            query.Parameters.AddWithValue("@ID_usuario", usuario);
+
+            //5-ejecutamos la consulta
+            SqlDataReader reader = query.ExecuteReader();
+            while (reader.Read()) //mientras haya un registro para leer
+            {
+                //creo libreta, le completo los datos 
+                Libreta libreta = new Libreta();
+                libreta.ID_libreta = (int)reader["ID_libreta"];
+                libreta.NameLibreta = reader["NameLibreta"].ToString();
+
+                libretas.Add(libreta);
+            }
+
+            //CERRAR EL READER AL TERMINAR DE LEER LOS REGISTROS
+            reader.Close();
+            //CERRAR LA CONEXION AL TERMINAR!!!!
+            conexion.Close();
+
+            return libretas;
 
 
-            return serviosOT;
+
         }
-
-
     }
+
 }
